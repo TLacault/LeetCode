889638c84c5b8905279c8a5d310578303935758b
Tim Lacault <72010776+TLacault@users.noreply.github.com>
2023-11-07 23:49:20 +0100

    Time: 0 ms (100.00%) | Memory: 9.2 MB (15.22%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector<int> array;
+        array.reserve(nums.size());
+        int k = 0;
+        for (const auto &num : nums) {
+            if (num != val) {
+                k++;
+                array.emplace_back(num);
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            nums[i] = array[i];
+        }
+        return k;
+    }
+};
