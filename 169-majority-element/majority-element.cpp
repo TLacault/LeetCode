c159c7a5aa34165e45215c4b874786ecf7964ec2
Tim Lacault <72010776+TLacault@users.noreply.github.com>
2023-11-10 18:02:39 +0100

    Time: 19 ms (17.21%) | Memory: 20 MB (23.88%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int, int> m;
+        for (auto i : nums) {
+            m[i]++;
+            if (m[i] > nums.size() / 2) return i;
+        }
+        return m.begin()->first;
+    }
+};
