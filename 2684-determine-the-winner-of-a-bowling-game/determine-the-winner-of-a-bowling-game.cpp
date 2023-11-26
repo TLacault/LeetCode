8ae42ceed610acda04297073f9c2e8c81a3fe9bd
Tim Lacault <72010776+TLacault@users.noreply.github.com>
2023-11-26 15:40:53 +0100

    Time: 36 ms (30.72%) | Memory: 72.4 MB (21.82%) - LeetSync

--- 2684-determine-the-winner-of-a-bowling-game/determine-the-winner-of-a-bowling-game.cpp
+++ 2684-determine-the-winner-of-a-bowling-game/determine-the-winner-of-a-bowling-game.cpp
@@ -10,10 +10,8 @@ public:
         for (int i = 0; i < player1.size(); i++) {
             score_1 += (double_1-- > 0) ? 2 * player1[i] : player1[i];
             if (player1[i] == 10) double_1 = 2;
-            cout << score_1 << endl;
             score_2 += (double_2-- > 0) ? 2 * player2[i] : player2[i];
             if (player2[i] == 10) double_2 = 2;
-            cout << score_2 << endl;
         }
         if (score_1 == score_2) return 0;
         if (score_1 > score_2) return 1;
