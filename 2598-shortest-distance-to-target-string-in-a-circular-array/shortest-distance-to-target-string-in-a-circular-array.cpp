69f0016ad706ae01cc923132c617702a15f5fbfe
Tim Lacault <72010776+TLacault@users.noreply.github.com>
2023-11-26 12:25:22 +0100

    Time: 18 ms (97.87%) | Memory: 14.9 MB (77.81%) - LeetSync

--- 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.cpp
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -4,16 +4,11 @@ public:
         int right = 0;
         for (int i = 0; i < words.size(); i++) {
             if (words[(startIndex + i) % words.size()] == target) {
-                right = i; break;
-            }
-            if (i == words.size() - 1) return -1;
-        }
-        int left = 0;
-        for (int i = 0; i < words.size(); i++) {
-            if (words[(startIndex - i + words.size()) % words.size()] == target) {
-                left = i; break;
+                return i;
+            } else if (words[(startIndex - i + words.size()) % words.size()] == target) {
+                return i;
             }
         }
-        return min(left, right);
+        return -1;
     }
 };
