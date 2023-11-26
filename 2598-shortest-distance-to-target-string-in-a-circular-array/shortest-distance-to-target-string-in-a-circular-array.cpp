dba354f5244489ecb866ea7f0a09c64d37d4d897
Tim Lacault <72010776+TLacault@users.noreply.github.com>
2023-11-26 12:28:35 +0100

    Time: 21 ms (91.19%) | Memory: 14.9 MB (77.81%) - LeetSync

--- 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.cpp
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int closetTarget(vector<string>& words, string target, int startIndex) {
         for (int i = 0; i < words.size(); i++) {
-            if (words[(startIndex + i) % words.size()] == target)
+            if (words[(startIndex + i) % words.size()] == target) {
                 return i;
-            else if (words[(startIndex - i + words.size()) % words.size()] == target)
+            } else if (words[(startIndex - i + words.size()) % words.size()] == target) {
                 return i;
+            }
         }
         return -1;
     }
