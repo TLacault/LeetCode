fc79cae10f5e73e80e6d2e6c0132f396d3a86213
Tim Lacault <72010776+TLacault@users.noreply.github.com>
2023-12-24 17:33:36 +0100

    Time: 13 ms (28.16%) | Memory: 13.3 MB (13.56%) - LeetSync

--- 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.cpp
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<int> intersection(vector<vector<int>>& nums) {
         map<int, int> m;
-        for (auto num : nums[0]) m[num]++;
-
-        map <int, int> current;
-        for (auto n : nums) {
-            current.clear();
-            for (auto i : n) if (m.contains(i)) current[i]++;
-            m = current;
-        }
         vector<int> res;
-        for (auto n : m) res.emplace_back(n.first);
+
+        for(auto num : nums)
+            for (auto n : num)
+                m[n]++;
+
+        for (auto i : m)
+            if (i.second == nums.size())
+                res.emplace_back(i.first);
+
         return res;
     }
 };
